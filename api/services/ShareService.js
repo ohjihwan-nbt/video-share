12c0a0c00b74a6eade451fa7bf85fb5d0468235e
ohjihwan-nbt <oh.jihwan@nbt.com>
2019-03-14 12:12:28 +0900

    수정: ShareService의 getShareInformation 로직 수정
    - result를 undefined가 아닌 error에 대한 상세 처리가 가능하도록 객체로 변경
    - 요청받은 id에 대한 서버 요청 후 에러 핸들링을 별도 처리
    - 백엔드에서 API 요청 결과에 대해 로깅 후 result.error에 true 할당

--- api/services/ShareService.js
+++ api/services/ShareService.js
@@ -3,11 +3,18 @@ const axios = require('axios')
 module.exports = {
   async getShareInformation (id) {
     // id를 사용하여 서버에 데이터를 요청한다.
-    let result
+    const result = {}
 
     try {
-      result = await axios.get('http://api.fureweb.com/exchangeRate/USD').then(r => r.data)
-    } catch (e) {}
+      const response = await axios.get('http://api.smallbites.kr/c/v1/brands/1')
+
+      result.status = response.status
+      result.data = response.data
+    } catch (e) {
+      sails.log.error(e)
+
+      result.error = true
+    }
 
     return result
   }
