66c85c1562d15be8507f18db9d8152fc3be35bdc
ohjihwan-nbt <oh.jihwan@nbt.com>
2019-03-14 14:37:12 +0900

    수정: ShareService내 getShareInformation 메서드 임시 변경
    - 임시 응답 즉시 작성하도록 변경
    - 프로토콜에 맞춘 응답객체 작성 (winning_count 제외)

--- api/services/ShareService.js
+++ api/services/ShareService.js
@@ -6,10 +6,32 @@ module.exports = {
     const result = {}
 
     try {
-      const response = await axios.get('http://api.smallbites.kr/c/v1/brands/1')
+      // qiz-­api.lockcast.co.kr/api/play_contents/${id}/share
+      // const response = await axios.get(`http://qiz-­stg-api.lockcast.co.kr/api/play_contents/${id}/share`)
 
-      result.status = response.status
-      result.data = response.data
+      // // 임시 사용 경로 (http 요청 테스트)
+      // const response = await axios.get(`http://api.fureweb.com/exchangeRate/USD`)
+
+      // // 응답 바인딩
+      // result.status = response.status
+      // result.data = response.data
+
+      // 임시 응답
+      result.status = 200
+      result.data = {
+        /** http 응답의 data.content가 실제로는 data가 된다. */
+        id: 1,
+        title: '구제역은?',
+        photo_url: 'https://cdn.smallbites.kr/znNu7qDdNgC5Ztk74K7J8qGqd08WcqbM',
+        video_thumbnail_url: 'https://alock-image.cashslide.kr/qiz/video/thumb_video_1552377563ccc89cc460.png',
+        share_video_url: 'https://alock-image.cashslide.kr/qiz/video/share_video_15523775653a02846805.mp4',
+        creator: {
+          id: 3,
+          name: 'FUREWEB',
+          referrer: 'mi19mj',
+          winning_count: 10
+        }
+      }
     } catch (e) {
       sails.log.error(e)
 
